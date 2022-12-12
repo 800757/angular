e32642fd65e0eb47999669648726c7d7aa7c892a
800757 <108819456+800757@users.noreply.github.com>
2022-12-12 11:37:56 +0300

    Update home.component.ts

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,7 +6,22 @@ import { EquipmentDataService } from 'src/app/core/services/equipment-data.servi
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  equipments$ = this.eventService.getData();
+export class HomeComponent implements OnInit {
+  equipments: Array<Equipment> = [] as any;
+  subscriptions: Array<Subscription> = [] as any;
   constructor(private eventService: EquipmentDataService) { }
+  
+  ngOnInit(): void {
+    this.subscriptions.push(
+        this.eventService.getData().subscribe(data => {
+      this.equipments = data;
+    })
+    );
+  }
+  
+  ngOnDestroy: void {
+    this.subscriptions.forEach(s => {
+      s.unsubscribe();
+    });
+  }
 }
